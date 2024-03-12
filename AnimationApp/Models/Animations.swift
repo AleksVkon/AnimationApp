40e6a3e76430351ead49a07cef7b23721f05dae9
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-03-12 11:45:29 +0530

    base

---
+++ AnimationApp/Models/Animations.swift
@@ -0,0 +1,8 @@
+//
+//  Animations.swift
+//  AnimationApp
+//
+//  Created by Aleksey Konchyts on 12.03.24.
+//
+
+import Foundation
---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,10 @@
+//
+//  DataStore.swift
+//  AnimationApp
+//
+//  Created by Aleksey Konchyts on 12.03.24.
+//
+
+import Foundation
+
+let animation = ["pop", "slideLeft", "slideRight", "slideDown", "slideUp", "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp", "fadeIn", "fadeOut", "fadeOutIn", "fadeInLeft", "fadeInRight", "fadeInDown", "fadeInUp", "zoomIn", "zoomOut", "fall", "shake", "flipX", "flipY", "morph", "squeeze", "flash", "wobble", "swing"]
